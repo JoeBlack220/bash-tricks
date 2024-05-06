179e2fe34530a9aa50919a7528f76e6104a75d5b
JoeBlack220 <348653205@qq.com>
2024-05-05 17:49:51 -0700

    add special parameter values starting with $

---
+++ variables/special_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "The exit code of the last command is $?"
+
+echo "The PID of the current shell is $$"
+
+echo "The last parameter of the last command is $_"
+
+echo "The PID of the last background process is $!"
+
+echo "The name of the current shell is $0"
+
+echo "The starting parameters of the current shell is $-"
+
+echo "The number of parameters of the current script is $#"
+
+echo "The value of the parameters of the current script is $@"
